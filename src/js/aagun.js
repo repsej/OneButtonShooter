c34b0de0c7d9cb98d696fcf36d1e685c7bcfb9ec
Jesper Rasmussen <jesper42@gmail.com>
2025-08-27 15:21:50 +0200

    Ship aa guns works

--- src/js/aagun.js
+++ src/js/aagun.js
@@ -1,30 +1,31 @@
 /** @format */
 
 class AAGun extends Enemy {
-
 	constructor(pos, tileIndex) {
 		super(pos, vec2(1), tile(tileIndex));
 
 		this.setCollision(true, true, false);
 		this.hp = 1;
-
-		this.framesToShoot = rand(50,100);
 	}
 
 	update() {
 		super.update(); 
 
-		if (this.pos.x < cameraPos.x + cameraSize.x/2 + 2 && this.hp > 0)
-		{
-			if(this.framesToShoot-- < 0 && this.pos.x > cameraPos.x)
-			{
-				this.framesToShoot = rand(100,150);
-			
-				let bulletSpeed = vec2(-.07,.07);
-				const bulletPos = this.pos.add(bulletSpeed.normalize(1));
-				new Bullet(bulletPos, bulletSpeed, this);
-			}
+		aaUpdateCannon(this);
+	}
+}
+
+function aaUpdateCannon(gun) {
+	if (gun.framesToShoot === undefined) gun.framesToShoot = rand(50,100);
+
+	if (gun.pos.x < cameraPos.x + cameraSize.x / 2 + 2 && gun.hp > 0) {
+		if (gun.framesToShoot-- < 0 && gun.pos.x > cameraPos.x) {
+			gun.framesToShoot = rand(100, 150);
+
+			let bulletSpeed = vec2(-.07, .07);
+			const bulletPos = gun.pos.add(bulletSpeed.normalize(1));
+			new Bullet(bulletPos, bulletSpeed, gun);
 		}
 	}
-
 }
+
--- src/js/shipPart.js
+++ src/js/shipPart.js
@@ -19,27 +19,25 @@ class ShipPart extends Enemy {
 		this.setCollision(true, true, false);
 		this.deathGravity = .01;
 		shipParts.push(this);
-		}
 
-	update()
-	{
+		this.isAAGun = isAAGun;
+	}
+
+	update() {
 		super.update();
 		this.angle = 0;
 		this.velocity.y = max(this.velocity.y, -.1);
 
 		this.velocity.x = min(this.velocity.x, .1);
 
-
-		if (shipHp <= 0) return;
+		if (this.isAAGun) aaUpdateCannon(this);
 	}
 
-	superHit(dam)
-	{
+	superHit(dam) {
 		super.hit(dam);
 	}
 
-	hit(dam=1)
-	{
+	hit(dam=1) {
 		if (this.hp < 0 || shipHp < 0){
 			return;
 		}
@@ -55,9 +53,9 @@ class ShipPart extends Enemy {
 		if (shipHp < 0)
 		{
 			for (const s of shipParts) {
+				s.velocity.x = 0;
 				s.superHit(s.hp);
 			}
 		}
 	}
-
 }
