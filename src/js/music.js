9cf2a04db2b31cc2ca3db0a3756d274a83b4571d
Jesper Rasmussen <jesper42@gmail.com>
2025-08-28 01:19:58 +0200

    Nicer music

--- src/js/music.js
+++ src/js/music.js
@@ -8,11 +8,22 @@ let instrumentList = [];
 
 let instrumentParams = [
 	[1, 0, 43, 0.01, 0.5, 0.5, , 0.5], // 0 bass
-	[20, 0, 170, 0.003, , 0.008, , 0.97, -35, 53, , , , , , 0.1], // 1 bass drum
-	[0.8, 0, 270, , , 0.12, 3, 1.65, -2, , , , , 4.5, , 0.02], // 2 snare
-	[1.1, 0, 77, , 0.3, 0.7, 2, 0.41, , , , , , , , 0.06], // 3 melody lead
-	[1.5, , 400, , , 0.5, 2, 0.1, , 1, , , , 2.5, , 0.5, , 0.5, 0.1], // 4 crash
+	[10, 0, 170, 0.003, , 0.008, , 0.97, -35, 53, , , , , , 0.1], // 1 bass drum
+	[0.6, 0, 270, , , 0.12, 3, 1.65, -2, , , , , 4.5, , 0.02], // 2 snare
+	[1.5, 0, 130, .1, .2,
+	 .2, 2, 1.4, , , 
+	 , , , .3, , 
+	 .2, .18, .7, .14, ,922], // 3 melody lead
+	[1, , 400, , , 0.5, 2, 0.1, , 1, , , , 2.5, , 0.5, , 0.5, 0.1], // 4 crash
 ];
+
+	//[1.1, .05, 77, , 0.3, 0.9, 2, 0.41, , , , , , , , 0.1], // 3 melody lead
+
+
+    // volume = 1, randomness = .05, frequency = 220, attack = 0, sustain = 0,  	  // 5 
+    // release = .1, shape = 0, shapeCurve = 1, slide = 0, deltaSlide = 0,			  // 5
+    // pitchJump = 0, pitchJumpTime = 0, repeatTime = 0, noise = 0, modulation = 0,   // 5
+    // bitCrush = 0, delay = 0, sustainVolume = 1, decay = 0, tremolo = 0, filter = 0 // 6
 
 
 let songData = [
@@ -22,13 +33,20 @@ let songData = [
 	80, // BPM
 ];
 
-function unfoldPattern(instrument, pan, startnode, pattern, starts) {
+function unfoldPattern(instrument, pan, startnode, pattern, starts, altPattern=undefined, altIndex=0) {
 	let nodes = [];
 	nodes.push(instrument);
 	nodes.push(pan);
 
-	for (const s of starts) {
-		for (const b of pattern) {
+	for (let i = 0; i < starts.length; i++) {
+		const s = starts[i];
+
+		let p = pattern;
+		if (altPattern && i == altIndex) {
+			p = altPattern;
+		}
+
+		for (const b of p) {
 			nodes.push(startnode + b + s);
 		}
 	}
@@ -46,7 +64,7 @@ function createMusic(level) {
 
 	// prettier-ignore
 	let chordStarts = [
-		, 0, 0, 5, 0, 7, 0, ,
+		, , 0, 0, 5, 0, 7, 0,
 	];
 
 	patterns = [[], []];
@@ -59,10 +77,23 @@ function createMusic(level) {
 		7, 0, 4, 7, 
 		p, 7, p, p]; // in the mood melody
 
-	let melNodes = unfoldPattern(3, -0.2, root + 12, melPattern, chordStarts);
+	let altMelPattern = [
+		0, 0, 0, 0, 
+		0, 0, 0, 0, 
+		0, 0, p, 0, 
+		p, p, p, p]; // in the mood melody
+
+
+	let altMelPattern2 = [
+		12, 12, 12, 12, 
+		12, 12, 12, 12, 
+		12, 12, p, 12, 
+		p, p, p, p]; // in the mood melody
+
+	let melNodes = unfoldPattern(3, -0.2, root + 12 + .2, melPattern, chordStarts, altMelPattern, 7);
 	patterns[0].push(melNodes);
 
-	let melNodes2 = unfoldPattern(3, 0.2, root + 12 + 12, melPattern, chordStarts);
+	let melNodes2 = unfoldPattern(3, 0.2, root + 12 - .2, melPattern, chordStarts, altMelPattern2, 7);
 	patterns[0].push(melNodes2);
 
 
@@ -91,11 +122,20 @@ function createMusic(level) {
 		p, p, 0, 0,
 	];
 
+	// let bdPattern = [0, p, 0, p, 0, p, 0, p];
+	// let snarePattern = [
+	// 	p, 0, p, 0,
+	// 	p, 0, p, 0,
+	// 	p, 0, p, 0,
+	// 	p, 0, p, 0,
+	// ];
+
+
 	patterns[0].push(unfoldPattern(1, 0, 7, bdPattern, bdStarts));
 	patterns[0].push(unfoldPattern(2, 0.1, 7, snarePattern, snareStarts));
 }
 
-let vol = .5;
+let musicVol = .3;
 let musicOn = true;
 
 let musicStartTime = 0;
@@ -106,7 +146,7 @@ function musicInit(level) {
 	musicLastPlayedBeat = -1;
 }
 
-const tempo = 6;
+const tempo = 5.5;
 
 
 let musicLastPlayedBeat = -1;
@@ -132,10 +172,7 @@ function musicUpdate() {
 
 	if (!musicOn) return;
 
-	//console.log("beat", beat);
-
-	// crashes
-	if (beat % (musicSongLength / 4) == 0) musicPlayCrash(beat == 0 ? 2.5 : 1);
+	if (beat == 0) musicPlayCrash(2);
 
 	for (const pat of patterns[0]) {
 		let instrument = instrumentList[pat[0]];
@@ -146,7 +183,7 @@ function musicUpdate() {
 			// console.log("semi", semitone);
 
 			//instrument.playNote(semitone - 12, vec2(cameraPos.x + pan, cameraPos.y), vol);
-			instrument.playNotePure(semitone - 12, pan, vol);
+			instrument.playNotePure(semitone - 12, pan, musicVol);
 		}
 	}
 }
@@ -158,5 +195,5 @@ function createInstruments() {
 }
 
 function musicPlayCrash(strength = 2) {
-	instrumentList[4].playNotePure(12, 0, vol * strength);
+	instrumentList[4].playNotePure(12, 0, musicVol * strength);
 }
